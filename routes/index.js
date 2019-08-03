2e848db0039e38b6648ff65d7f065817c9f89b0a
ATarsa <smb909@gmail.com>
2019-08-03 09:43:39 +0100

    Format date format

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+// helper for data format
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
 
-const messages = [ { text: 'Hi there!', user: 'Amando', added: new Date() }, { text: 'Hello World!', user: 'Charles', added: new Date() } ];
+const messages = [ { text: 'Hi there!', user: 'Amando', added: new Date().toLocaleDateString("en-UK", options) }, { text: 'Hello World!', user: 'Charles', added: new Date().toLocaleDateString("en-UK", options) } ];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,7 +15,7 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/new', function(req,res, next){
-  let msg = {text: req.body.msg, user: req.body.username, added: new Date() };
+  let msg = {text: req.body.msg, user: req.body.username, added: new Date().toLocaleDateString("en-UK", options) };
   messages.push(msg);
 
   res.redirect('/'); 
