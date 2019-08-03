a7e9f26260af8830c8d4c2c8c87ac324d7187189
ATarsa <smb909@gmail.com>
2019-08-03 09:24:43 +0100

    Add new message route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+
+const messages = [ { text: 'Hi there!', user: 'Amando', added: new Date() }, { text: 'Hello World!', user: 'Charles', added: new Date() } ];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+
+router.post('/new', function(req,res, next){
+  let msg = {text: req.body.msg, user: req.body.username, added: new Date() };
+  messages.push(msg);
+
+  res.redirect('/'); 
+
 });
 
 module.exports = router;
